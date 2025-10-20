cad11212c6f8a5918aec70a24388dfdc23a8bf81
shadrackbond <stellamusembi10@gmail.com>
2025-10-20 21:15:58 +0300

    implementing echo to print the text after it to the terminal

--- app/main.js
+++ app/main.js
@@ -16,6 +16,15 @@ function prompt() {
       process.exit(0);
     }
     let myAnswer = `${answer}: command not found`
+// am thinking of making echo a variable
+    const argument = 'echo';
+
+    if(answer.includes(argument)){
+      //const part = answer.substring(0,8);
+      const newAnswer = answer.replace(argument,"") + '\n';
+      myAnswer = newAnswer
+    }
+    //what if I do the output without the argument lie new answer - argument
     console.log(myAnswer)
 
     prompt();
